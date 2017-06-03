08ca46d91c3aa1f937000f5d0aa5513e81db137f
s952163 <s995110@yahoo.co.jp>
2017-06-04 08:55:05 +0900

    Add project files.

---
+++ BitCoinFun/BitCoin.fsx
@@ -0,0 +1,14 @@
+﻿#r @"..\packages\FSharp.Data.2.3.3\lib\net40\FSharp.Data.dll"
+
+open System.Net
+open FSharp.Data
+open FSharp.Data.JsonExtensions
+
+let url = """http://api.coindesk.com/v1/bpi/currentprice/USD.json"""
+let client =  new WebClient()
+let result = client.DownloadString(url)
+printfn "%s" result
+
+let json = JsonValue.Parse(result)
+let fx = json?bpi?USD?rate_float
+
